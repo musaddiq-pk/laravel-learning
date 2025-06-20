82fd9dcf3e50cad978b1f9e6039ad4211dcc3f37
musadiq-ciklum <musaddiq.ur.rehman@ciklum.com>
2025-06-20 18:14:22 +0500

    Creating First Filament Crud

---
+++ app/app/Filament/Resources/TemplateResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TemplateResource\Pages;
+use App\Filament\Resources\TemplateResource\RelationManagers;
+use App\Models\Template;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TemplateResource extends Resource
+{
+    protected static ?string $model = Template::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\Textarea::make('description')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('description')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('created_at')->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTemplates::route('/'),
+            'create' => Pages\CreateTemplate::route('/create'),
+            'edit' => Pages\EditTemplate::route('/{record}/edit'),
+        ];
+    }
+}
